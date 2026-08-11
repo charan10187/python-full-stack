e1c8390c3d6a08b153a6fe7063403f1f60a532ea
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-11 19:52:29 +0530

    maximum and minimum value of array

--- python/Day3.py
+++ python/Day3.py
@@ -1,6 +1,9 @@
 arr=[10,20,30,40,50,60,70,80,90]
-max=arr[0]
+maximum=arr[0]
+minimum=arr[0]
 for i in range(len(arr)):
-    if arr[i]> max: 
-        max=arr[i]
-print(max)
+    if arr[i]> maximum: 
+        maximum=arr[i]
+    if arr[i]<minimum:
+        minimum=arr[i]
+print("max=",maximum,"minimum=",minimum)
