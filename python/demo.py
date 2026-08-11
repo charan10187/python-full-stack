b7b628041a8b133df771ad2d2813b1c88d16fea9
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-11 12:14:40 +0530

    learned the div and span in HTML and created Norway flag

---
+++ python/demo.py
@@ -0,0 +1,21 @@
+# LIST=[]
+# num=[10,20,30,40,50]
+# # print(num[0])
+# # print(num[1])
+# # print(num[2])
+# # print(num[3])
+# for _ in range(0,3):
+#     print(num[_])
+#     sum=0
+#     sum=sum + num[_]
+#  TUPLE
+marks={
+    "name":"charan",
+    "age":[22,3,4,5,66,3,78],
+    "city":"hyd"
+}
+print(marks["name"])
+print(marks["age"][4])
+
+
+
