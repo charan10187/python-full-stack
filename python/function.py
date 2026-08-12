720971c809699af922c6547142249c57093fa9e4
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-12 20:06:39 +0530

    checking weather the student has passed the exam or not

--- python/function.py
+++ python/function.py
@@ -1,3 +1,16 @@
-def fun(a,b):
-    print(a+b)
-fun(10,10)
+# def fun(a,b):
+#     print(a+b)
+# fun(10,10)
+
+# calculater.py
+# def calculater()
+
+def result(marks):
+    if marks<35:
+        print("fail")
+    elif marks>35 and marks<90:
+        print("pass")
+    else:
+        print("topper")
+marks=int(input("enter value: "))
+result(marks)
