fc84a2498aafe137c055b5981bca0d2fe0960ca6
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-14 22:58:59 +0530

    python(DSA) practice folder is moved to another repo

--- python/Practice(DSA_Basics)/p1.py
+++
@@ -1,5 +0,0 @@
-# Problem 1: Print numbers from 1 to N
-
-N=int(input("N = "))
-for i in range(1,N+1):
-    print(i)
--- python/Practice(DSA_Basics)/p2.py
+++
@@ -1,5 +0,0 @@
-# Problem 2 — Print numbers from N to 1
-
-N=int(input("N = "))
-for i in range(N,0,-1):
-    print(i)
