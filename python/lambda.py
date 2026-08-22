7478e8968bad2f3fd4ff34ac3a0096b1c4b63427
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-22 19:57:55 +0530

    lambda practice 2

--- python/lambda.py
+++ python/lambda.py
@@ -1,5 +1,13 @@
 '''
-   single line function or shortest form of code
+   Lambda is known as anonymous functions meaning 
+   they do not require a function name
+   * lambda can contain only one expression
+   * the expression's result is returned automatically
+   
+   * lambda is commonly used with 
+      map(),
+      filter(),
+      sorted().
 
 '''
 
@@ -10,9 +18,13 @@ print(squ(5))
 sum=lambda a,b: a+b
 print(sum(10,20))
 
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = list(filter(lambda number: number % 2 == 0, numbers))
 
+print(even_numbers)  # [2, 4, 6]
 
-# map
-# filter
+result=list(map(lambda x:x*2,even_numbers))
+
+print(result)
 
 
