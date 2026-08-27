9a1dece0f03125a324c0f5c0542d254bc595c77b
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-27 09:51:07 +0530

    get method

--- python/Day08-25/2D.py
+++ python/Day08-25/2D.py
@@ -3,3 +3,8 @@
 matrix=[[2*x for x in range(0,5)] for x in range(0,4)]
 
 print(matrix)
+
+for i in range(len(matrix)):
+    for j in range(len(matrix)):
+        print(matrix[i][j],end=" ")
+    print()
--- python/testing.py
+++ python/testing.py
@@ -4,10 +4,11 @@
 
 
 
-li=[10,20,30,40,50,60]
-for i in range(len(li)):
-    print(i)
-for index, val in enumerate(li):
-    print(index,val)
+# li=[10,20,30,40,50,60]
+# for i in range(len(li)):
+#     print(i)
+# for index, val in enumerate(li):
+#     print(index,val)
 
 
+n=int(input("power = "))
