bcc7f451ac9fcddd3cc79997ab7171f555eca2a4
Sri Charan Surepalli <charan10187@gmail.com>
2026-09-03 20:21:47 +0530

    exception notes

--- Backend/Day09-03/exception.py
+++ Backend/Day09-03/exception.py
@@ -1,8 +1,19 @@
 try:
-    a=10
-    b=0
-    print(a/b)
-except:
-    print("error occurs")
+    a = 10
+    b = 0
+    print(a / b)
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
 finally:
-    print("runed")
+    print("This block always runs.")
+
+try:
+    number=int(input("enter numbere:"))
+    result=10/number
+    print(result)
+except ValueError:
+    print("please enter a valid number")
+except ZeroDivisionError:
+    print("number cannot be zero")
+except:
+    print("somthing is wrong")
