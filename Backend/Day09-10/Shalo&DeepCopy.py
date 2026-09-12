643c6aee801928133d1e09fc88ece2da48f52c30
Sri Charan Surepalli <charan10187@gmail.com>
2026-09-12 20:05:44 +0530

    rename file

---
+++ Backend/Day09-10/Shalo&DeepCopy.py
@@ -0,0 +1,23 @@
+# shalo copy and deep copy
+
+# import copy
+# a=[10,20,30]
+# b=a.copy() 
+# b.append(40)
+# print(a)   #[10,20,30,40]
+# print(b)   #[10,20,30,40]
+
+
+# a=[10,20,30]
+# b=a.copy() 
+# b.append(40)
+# print(a)    #[10,20,30]
+# print(b)    #[10,20,30,40]
+
+# a=[10,20,30]
+# b=a[:]
+# b[0]=100
+# print(a)  #[10,20,30]
+# print(b)  #[100,20,30]
+
+
