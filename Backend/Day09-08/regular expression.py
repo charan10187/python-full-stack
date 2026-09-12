023edec31ab21a583e48e34a91bf0cbcd267e588
Sri Charan Surepalli <charan10187@gmail.com>
2026-09-12 19:42:29 +0530

    update

---
+++ Backend/Day09-08/regular expression.py
@@ -0,0 +1,16 @@
+# # file and stream
+# # prefix and suffix
+
+
+# oops
+# regular expression (re model)
+# exception handling in Python
+# file or stream handling
+# os Module
+# multi threading
+# python logging
+# date and Time module
+# garbage collection
+# python data base
+
+
