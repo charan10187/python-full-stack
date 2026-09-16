d1b5208ef8e466ceb67511910e06429f3f742482
Sri Charan Surepalli <charan10187@gmail.com>
2026-09-16 20:47:40 +0530

    logging 2

--- Backend/Day09-16/Logging.py
+++ Backend/Day09-16/Logging.py
@@ -7,14 +7,24 @@
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-print()
-logging.info("logging code is running fine")
-print()
-logging.debug("logging code is running fine")
-print()
-logging.error("logging code is running fine")
-print()
-logging.warning("logging code is running fine")
-print()
-logging.critical("logging code is running fine")
+# # logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(filename="info.log",level=logging.DEBUG)
+# print()
+# logging.info("logging code is running fine")
+# print()
+# logging.debug("logging code is running fine")
+# print()
+# logging.error("logging code is running fine")
+# print()
+# logging.warning("logging code is running fine")
+# print()
+# logging.critical("logging code is running fine")
+
+logging.basicConfig(filename="division.log",level=logging.DEBUG)
+
+try:
+    a=10
+    b=0
+    result=a/b
+except ZeroDivisionError as e:
+    logging.error(e)
