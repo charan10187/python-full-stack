caa71f45e85fc5e60105d6862a618e401abfa26c
Sri Charan Surepalli <charan10187@gmail.com>
2026-09-16 20:24:53 +0530

    Logging in python

---
+++ Backend/Day09-16/Logging.py
@@ -0,0 +1,20 @@
+# INFO
+# DEBUG 
+# ERROR
+# WARNING
+# CRITICAL 
+
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+print()
+logging.info("logging code is running fine")
+print()
+logging.debug("logging code is running fine")
+print()
+logging.error("logging code is running fine")
+print()
+logging.warning("logging code is running fine")
+print()
+logging.critical("logging code is running fine")
